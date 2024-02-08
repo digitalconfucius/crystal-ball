a32e3d2f4b62b397e0ebd8a5f7cd5759278d1d69
digitalconfucius <156959605+digitalconfucius@users.noreply.github.com>
2024-02-08 18:26:15 -0500

    Still not working

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,14 @@
 Rails.application.routes.draw do
+  
   root "objects#index"
 
-  # get "/objects", to: "objects#index"
-
-  match 'index', to: 'objects#index', via: [:get, :post]
-
-  post 'submit_form', to: 'objects#submit_form'
-
+  get 'objects', to: 'objects#index'
+  post 'objects', to: 'objects#index'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
